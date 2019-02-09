4393dad8bb1ec543f08bf852e9b13e9e0ad9701c
arthur <arthur.teisseire@epitech.eu>
2019-02-09 19:33:06 +0100

    Remove useless ;

--- src/symbol.c
+++ src/symbol.c
@@ -12,7 +12,7 @@ void print_symbol(const Elf64_Ehdr *hdr, const Elf64_Shdr *current_section, int 
 {
     Elf64_Shdr *strtab = get_section(hdr, current_section->sh_link);
     Elf64_Sym *sym = get_symbol((void *) hdr + current_section->sh_offset, j);
-    char *name = (char *) hdr + strtab->sh_offset + sym->st_name;;
+    char *name = (char *) hdr + strtab->sh_offset + sym->st_name;
 
     if ((ELF64_ST_BIND(sym->st_info) & STT_OBJECT) || ELF64_ST_BIND(sym->st_info) & STT_FUNC)
         printf("symbol name : %s\n", name);
