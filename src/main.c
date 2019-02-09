71e34459cabff71d451f67fe8f5ff751a18e9ed7
arthur <arthur.teisseire@epitech.eu>
2019-02-09 22:47:22 +0100

    Check Elf magic number

--- src/main.c
+++ src/main.c
@@ -5,7 +5,20 @@
 ** main.c
 */
 
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include "nm.h"
+
+void check_elf_format(Elf64_Ehdr *hdr, const char *filename)
+{
+    const char magic[] = {0x7f, 'E', 'L', 'F'};
+
+    if (memcmp(hdr->e_ident, magic, 4) != 0) {
+        fprintf(stdout, "nm: %s: file format not recognized\n", filename);
+        exit(84);
+    }
+}
 
 int main(int ac, char **av)
 {
@@ -14,6 +27,7 @@ int main(int ac, char **av)
     if (ac != 1) {
         hdr = file_to_hdr(av[1]);
         if (hdr) {
+            check_elf_format(hdr, av[1]);
             print_file_symbols(hdr);
             return (0);
         }
