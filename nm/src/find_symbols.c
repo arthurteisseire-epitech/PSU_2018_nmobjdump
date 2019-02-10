aa3cde482a528a9c9f2925d246c07b53a0ecd98d
arthur <arthur.teisseire@epitech.eu>
2019-02-10 18:05:36 +0100

    Sort seems to be the same as nm

--- nm/src/find_symbols.c
+++ nm/src/find_symbols.c
@@ -17,12 +17,16 @@ static int my_strcmp(const char *s1, const char *s2)
 
     while (s1[i] && s2[i] && tolower(s1[i]) == tolower(s2[i]))
         ++i;
-    if (i > 0 && tolower(s1[i - 1]) == tolower(s2[i - 1])) {
-        if (!isalpha(s1[i]))
-            return (1);
-        else if (!isalpha(s2[i]))
-            return (-1);
-    }
+    if (s1[i] == '\0' && s2[i] == '\0')
+        return (0);
+    if (s1[i] != '\0' && !isalpha(s1[i]))
+        return (1);
+    if (s1[i] != '\0' && !isalpha(s2[i]))
+        return (-1);
+    if (s1[i] == '\0')
+        return (-1);
+    if (s2[i] == '\0')
+        return (1);
     return (tolower(s1[i]) - tolower(s2[i]));
 }
 
