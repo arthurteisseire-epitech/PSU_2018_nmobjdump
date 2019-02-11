62cb73c361a73004cb443f6267955603797dab96
arthur <arthur.teisseire@epitech.eu>
2019-02-11 22:21:05 +0100

    Extract condition to print section into a function

--- objdump/src/sections.c
+++ objdump/src/sections.c
@@ -6,8 +6,19 @@
 */
 
 #include <stdio.h>
+#include <stdbool.h>
 #include "objdump.h"
 #include "lib.h"
+
+static bool is_section_printable(const Elf64_Shdr *current_section)
+{
+    return (current_section->sh_type != SHT_NOBITS &&
+            current_section->sh_type != SHT_NULL &&
+            current_section->sh_type != SHT_RELA &&
+            current_section->sh_type != SHT_SYMTAB &&
+            current_section->sh_type != SHT_STRTAB &&
+            current_section->sh_size != 0);
+}
 
 void print_section(Elf64_Ehdr *hdr, Elf64_Shdr *shdr)
 {
@@ -19,12 +30,7 @@ void print_sections(Elf64_Ehdr *hdr)
     Elf64_Shdr *current_section = get_section_header(hdr);
 
     for (size_t i = 0; i < hdr->e_shnum; ++i) {
-        if (current_section->sh_type != SHT_NOBITS &&
-            current_section->sh_type != SHT_NULL &&
-            current_section->sh_type != SHT_RELA &&
-            current_section->sh_type != SHT_SYMTAB &&
-            current_section->sh_type != SHT_STRTAB &&
-            current_section->sh_size != 0)
+        if (is_section_printable(current_section))
             print_section(hdr, current_section);
         current_section = (void *) current_section + hdr->e_shentsize;
     }
