bb3c987bfaffc70a677f3a91c02cbafc78960f3c
arthur <arthur.teisseire@epitech.eu>
2019-02-11 22:15:51 +0100

    Add check to print only necessary sections

--- objdump/src/sections.c
+++ objdump/src/sections.c
@@ -19,7 +19,13 @@ void print_sections(Elf64_Ehdr *hdr)
     Elf64_Shdr *current_section = get_section_header(hdr);
 
     for (size_t i = 0; i < hdr->e_shnum; ++i) {
-        print_section(hdr, current_section);
+        if (current_section->sh_type != SHT_NOBITS &&
+            current_section->sh_type != SHT_NULL &&
+            current_section->sh_type != SHT_RELA &&
+            current_section->sh_type != SHT_SYMTAB &&
+            current_section->sh_type != SHT_STRTAB &&
+            current_section->sh_size != 0)
+            print_section(hdr, current_section);
         current_section = (void *) current_section + hdr->e_shentsize;
     }
 }
