51a523bca788f5c9a1b13e21f94e9e5b2dad12fc
arthur <arthur.teisseire@epitech.eu>
2019-02-16 09:40:42 +0100

    Use function pointer for exec

--- nm/src/main.c
+++ nm/src/main.c
@@ -11,7 +11,7 @@
 #include "nm.h"
 #include "lib.h"
 
-static void exec_with(const void *hdr, const char *filename, int ac)
+static void arch_exec(const void *hdr, const char *filename, int ac)
 {
     if (ac >= 3)
         printf("\n%s:\n", filename);
@@ -19,15 +19,16 @@ static void exec_with(const void *hdr, const char *filename, int ac)
         printf("nm: %s: no symbols\n", filename);
 }
 
-int exec(int ac, const char **av, const char *prog_name)
+int exec(int ac, const char **av, const char *prog_name,
+void (*ex)(const void *, const char *, int))
 {
     void *hdr;
     int status = 0;
 
     for (int i = 1; i < ac; ++i) {
         hdr = file_to_hdr(prog_name, av[i]);
-        if (hdr && check_supported(hdr, NULL) != 84)
-            exec_with(hdr, av[i], ac);
+        if (hdr && check_supported(hdr, NULL) == 0)
+            ex(hdr, av[i], ac);
         else
             status = 84;
     }
@@ -39,6 +40,6 @@ int main(int ac, const char **av)
     const char *prog_name = "nm";
 
     if (ac == 1)
-        return (exec(2, (const char *[]) {"", "a.out"}, prog_name));
-    return (exec(ac, av, prog_name));
+        return (exec(2, (const char *[]) {"", "a.out"}, prog_name, arch_exec));
+    return (exec(ac, av, prog_name, arch_exec));
 }
--- objdump/src/main.c
+++ objdump/src/main.c
@@ -9,14 +9,15 @@
 #include "objdump.h"
 #include "lib.h"
 
-static void exec_with_arch(const void *hdr, const char *filename,
+static void arch_exec(const void *hdr, const char *filename,
 __attribute((unused)) int ac)
 {
     print_header(hdr, filename);
     print_sections(hdr, ((Elf64_Ehdr *) hdr)->e_shnum);
 }
 
-static int exec(int ac, const char **av, const char *prog_name)
+int exec(int ac, const char **av, const char *prog_name,
+void (*ex)(const void *, const char *, int))
 {
     void *hdr;
     int status = 0;
@@ -24,7 +25,7 @@ static int exec(int ac, const char **av, const char *prog_name)
     for (int i = 1; i < ac; ++i) {
         hdr = file_to_hdr(prog_name, av[i]);
         if (hdr && check_supported(hdr, av[i]) == 0)
-            exec_with_arch(hdr, av[i], ac);
+            ex(hdr, av[i], ac);
         else
             status = 84;
     }
@@ -36,6 +37,6 @@ int main(int ac, const char *av[])
     const char *prog_name = "objdump";
 
     if (ac == 1)
-        return (exec(2, (const char *[]) {"", "a.out"}, prog_name));
-    return (exec(ac, av, prog_name));
+        return (exec(2, (const char *[]) {"", "a.out"}, prog_name, arch_exec));
+    return (exec(ac, av, prog_name, arch_exec));
 }
