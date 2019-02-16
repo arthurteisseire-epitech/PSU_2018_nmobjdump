8ef13f85a839ed643cba6d62bac00004c67d536d
arthur <arthur.teisseire@epitech.eu>
2019-02-16 09:30:34 +0100

    Refactor objdump main

--- objdump/src/main.c
+++ objdump/src/main.c
@@ -9,21 +9,22 @@
 #include "objdump.h"
 #include "lib.h"
 
-static void exec_with_arch(const void *hdr, const char *filename)
+static void exec_with_arch(const void *hdr, const char *filename,
+__attribute((unused)) int ac)
 {
     print_header(hdr, filename);
     print_sections(hdr, ((Elf64_Ehdr *) hdr)->e_shnum);
 }
 
-static int exec(int ac, const char **av)
+static int exec(int ac, const char **av, const char *prog_name)
 {
     void *hdr;
     int status = 0;
 
     for (int i = 1; i < ac; ++i) {
-        hdr = file_to_hdr("objdump", av[i]);
+        hdr = file_to_hdr(prog_name, av[i]);
         if (hdr && check_supported(hdr, av[i]) == 0)
-            exec_with_arch(hdr, av[i]);
+            exec_with_arch(hdr, av[i], ac);
         else
             status = 84;
     }
@@ -32,7 +33,9 @@ static int exec(int ac, const char **av)
 
 int main(int ac, const char *av[])
 {
+    const char *prog_name = "objdump";
+
     if (ac == 1)
-        return (exec(2, (const char *[]) {"", "a.out"}));
-    return (exec(ac, av));
+        return (exec(2, (const char *[]) {"", "a.out"}, prog_name));
+    return (exec(ac, av, prog_name));
 }
