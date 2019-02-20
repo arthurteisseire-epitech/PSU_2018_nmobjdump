5f64062e7283c88e63042421205b5df1d5df5dcb
arthur <arthur.teisseire@epitech.eu>
2019-02-20 11:56:28 +0100

    Remove debug print

--- objdump/src/main.c
+++ objdump/src/main.c
@@ -13,7 +13,6 @@ static void arch_exec(const void *hdr, const char *filename,
 __attribute((unused)) int ac)
 {
     printf("\n%s:     file format elf64-x86-64\n", filename);
-    printf("%p\n", hdr);
     print_architecture(_M(hdr, e_machine));
     print_all_flags(hdr, _M(hdr, e_type), _M(hdr, e_shnum));
     print_address(_M(hdr, e_entry));
