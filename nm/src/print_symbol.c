bfb7d00cada83a074d1045cd5ea8ca6bf6899389
arthur <arthur.teisseire@epitech.eu>
2019-02-21 17:27:16 +0100

    Use * flag of printf to print different number of spaces

--- nm/src/print_symbol.c
+++ nm/src/print_symbol.c
@@ -11,19 +11,13 @@
 
 static void print_sym(const nm_t *nm, const void *hdr, size_t i)
 {
-    if (nm->symbols[i].type == 'U' || nm->symbols[i].type == 'w') {
-        if (get_arch(hdr) == 64)
-            printf("%18c %s\n", nm->symbols[i].type, nm->symbols[i].name);
-        else
-            printf("%10c %s\n", nm->symbols[i].type, nm->symbols[i].name);
-    } else {
-        if (get_arch(hdr) == 64)
-            printf("%016x %c %s\n", nm->symbols[i].value,
-                   nm->symbols[i].type, nm->symbols[i].name);
-        else
-            printf("%08x %c %s\n", nm->symbols[i].value,
-                   nm->symbols[i].type, nm->symbols[i].name);
-    }
+    int n = get_arch(hdr) == 64 ? 16 : 8;
+
+    if (nm->symbols[i].type == 'U' || nm->symbols[i].type == 'w')
+        printf("%*c %s\n", n + 2, nm->symbols[i].type, nm->symbols[i].name);
+    else
+        printf("%0*x %c %s\n", n, nm->symbols[i].value,
+        nm->symbols[i].type, nm->symbols[i].name);
 }
 
 void print_symbols(nm_t *nm, const void *hdr)
