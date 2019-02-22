b8219a03ceea4219a4d3db8c7ffc45d5397dde7c
arthur <arthur.teisseire@epitech.eu>
2019-02-22 11:13:01 +0100

    Remove useless architectures

--- objdump/src/print_header_utils.c
+++ objdump/src/print_header_utils.c
@@ -11,23 +11,7 @@
 
 static const map_t machines[] = {
         {EM_X86_64,      "i386:x86-64"},
-        {EM_NONE,        "None"},
-        {EM_M32,         "WE32100"},
-        {EM_SPARC,       "Sparc"},
         {EM_386,         "i386"},
-        {EM_68K,         "MC68000"},
-        {EM_88K,         "MC88000"},
-        {EM_860,         "Intel 80860"},
-        {EM_MIPS,        "MIPS R3000"},
-        {EM_PARISC,      "HPPA"},
-        {EM_SPARC32PLUS, "Sparc v8+"},
-        {EM_PPC,         "PowerPC"},
-        {EM_PPC64,       "PowerPC64"},
-        {EM_S390,        "IBM S/390"},
-        {EM_ARM,         "ARM"},
-        {EM_SH,          "Renesas / SuperH SH"},
-        {EM_SPARCV9,     "Sparc v9"},
-        {EM_IA_64,       "Intel IA-64"},
         {0, NULL}
 };
 
