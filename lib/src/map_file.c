499748ff7c345ca29760c93d5f44ed6b9ddc1519
arthur <arthur.teisseire@epitech.eu>
2019-02-22 15:54:05 +0100

    Handle empty file and size 1 file

--- lib/src/map_file.c
+++ lib/src/map_file.c
@@ -24,12 +24,10 @@ static void *fd_to_hdr(int fd, const char *filename, const char *prog)
 
     fstat(fd, &s);
     buf = mmap(NULL, (size_t) s.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
-    if (buf == MAP_FAILED) {
-        perror("mmap");
+    if (buf == MAP_FAILED)
         return (NULL);
-    }
     off = _E(buf, e_shoff) + _E(buf, e_shnum) * _E(buf, e_shentsize);
-    if (s.st_size < _E(buf, e_ehsize) || s.st_size < off) {
+    if (s.st_size == 1 || s.st_size < _E(buf, e_ehsize) || s.st_size < off) {
         fprintf(stderr, "%s: %s: File truncated\n", prog, filename);
         return (NULL);
     }
